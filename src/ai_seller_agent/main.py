c0e0d1b5a107416921cd770c05cbbf03c4ff3633
mrKazzila <mrkazzila@gmail.com>
2026-08-10 09:08:08 +0300

    feat: update application entrypoint

--- src/ai_seller_agent/main.py
+++ src/ai_seller_agent/main.py
@@ -1,3 +1,14 @@
-def main():
-    pass
+from ai_seller_agent.application import create_app
 
+app = create_app()
+
+
+def main() -> None:
+    import uvicorn
+
+    uvicorn.run(
+        "ai_seller_agent.main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True,
+    )
