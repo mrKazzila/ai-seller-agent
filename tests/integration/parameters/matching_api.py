5d966ac0fad1c523d558a677cb10d52a30b697c5
mrKazzila <mrkazzila@gmail.com>
2026-08-10 11:19:23 +0300

    test: add inter params

--- tests/integration/parameters/matching_api.py
+++ tests/integration/parameters/matching_api.py
@@ -5,16 +5,22 @@ import pytest
 
 def generate_invalid_match_request_data() -> list:
     requests = [
-        {"message": "exact"},
-        {},
-        {"messages": "exact"},
-        {"messages": [1]},
+        ({"message": "exact"}, "missing messages field"),
+        ({}, "empty object"),
+        ({"messages": "exact"}, "messages is string"),
+        ({"messages": [1]}, "message is integer"),
+        (None, "null body"),
+        ([], "array body"),
+        ({"messages": None}, "messages is null"),
+        ({"messages": [None]}, "message is null"),
+        ({"messages": [{}]}, "message is object"),
+        ({"messages": [True]}, "message is boolean"),
     ]
     return [
         pytest.param(
             request,
             HTTPStatus.UNPROCESSABLE_ENTITY,
-            id=f"payload: {request}, status: 422",
+            id=f"payload: {description}, status: 422",
         )
-        for request in requests
+        for request, description in requests
     ]
